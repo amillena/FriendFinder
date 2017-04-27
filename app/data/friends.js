5b20448728f868f5c752b0a8822989b3d66b361b
amillena <amillena@yahoo.com>
2017-04-26 17:49:58 -0700

    initial upload

---
+++ app/data/friends.js
@@ -0,0 +1,15 @@
+//will hold all of the data for new members
+
+var friends = [
+
+  {
+    name: "Anthony",
+    location:"Concord",
+    age: "40",
+    photoLink: "",
+    score: "",    
+  },
+
+]
+
+module.exports = friends; //makes table data available to other pages
